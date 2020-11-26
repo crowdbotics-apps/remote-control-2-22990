f7f62597fc2dd3e7d8878428912fe9e61e10b6d8
Crowdbotics <team@crowdbotics.com>
2020-11-26 12:30:49 +0000

    Update components

--- src/features/BlankScreen0178819/screens/blank.js
+++ src/features/BlankScreen0178819/screens/blank.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 import {
   View,
   Image,
@@ -8,33 +8,89 @@ import {
   Button,
   Switch,
   TextInput,
-  StyleSheet,
-} from "react-native";
-import DateTimePicker from 'react-native-datepicker';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Slider from '@react-native-community/slider';
-import { CheckBox } from 'react-native-elements';
-import {SlideMenuIcon} from '../../../navigator/slideMenuIcon';
+  StyleSheet
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 
 export default class Blank extends React.Component {
-  static navigationOptions = ({navigation}) => {
+  static navigationOptions = ({ navigation }) => {
     return {
-      headerLeft: <SlideMenuIcon navigationProps={navigation} />,
-    };
-  };
-  
-  state = {};
+      headerLeft: <SlideMenuIcon navigationProps={navigation} />
+    }
+  }
+
+  state = {}
 
   render = () => (
-    <View style={styles.container}>
-      <Text>This is your new component</Text>
-    </View>
-  );
+    <ImageBackground
+      source={{
+        uri:
+          "https://d3tklmlrp1a8c2.cloudfront.net/media/project_component_resources/126263_car-wallpaper-app.jpg"
+      }}
+      style={styles.ImageBackground_1}
+    >
+      <Icon
+        iconFont="FontAwesome Icons"
+        name="arrow-up"
+        style={styles.Icon_3}
+      />
+      <Icon
+        iconFont="FontAwesome Icons"
+        name="arrow-down"
+        style={styles.Icon_5}
+      />
+      <Icon name="arrow-left" style={styles.Icon_7} />
+      <Icon name="arrow-right" style={styles.Icon_9} />
+    </ImageBackground>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginHorizontal: 16,
+    marginHorizontal: 16
   },
-});
+  ImageBackground_1: { backgroundSize: "stretch" },
+  Icon_3: {
+    marginTop: 171,
+    marginBottom: 5,
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingTop: 5,
+    paddingBottom: 5,
+    alignSelf: "center",
+    fontSize: 20,
+    color: "#000000",
+    backgroundColor: "#ffffff",
+    fontStyle: "normal",
+    fontWeight: "normal",
+    textAlign: "left",
+    textAlignVertical: "center",
+    textDecorationLine: "none",
+    textTransform: "none",
+    lineHeight: 12,
+    letterSpacing: 0
+  },
+  Icon_5: {
+    marginTop: 24,
+    marginBottom: 5,
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingTop: 5,
+    paddingBottom: 5,
+    alignSelf: "center",
+    fontSize: 20,
+    textAlign: "left",
+    textAlignVertical: "center",
+    textDecorationLine: "none",
+    textTransform: "none",
+    lineHeight: 12,
+    letterSpacing: 0
+  },
+  Icon_7: { marginLeft: 109, marginTop: -64 },
+  Icon_9: { marginRight: 108, marginTop: -34, alignSelf: "flex-end" }
+})
